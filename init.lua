77951e47fbf0f1f4a2b6a5faf96bf928d3f254e0
Kevin Li <kevinli020508@gmail.com>
2020-12-18 17:50:18 +0800

    Convert to a spoon package

--- init.lua
+++ init.lua
@@ -1,77 +1,94 @@
-local eventtap = require("hs.eventtap") 
-local keycodes = require("hs.keycodes")
-local events = eventtap.event.types
+--- === TouchCursor ===
+--- Speed up text edits and navigations by using the space bar
+--- as a modifier key (but still lets you type spaces).
+--- Allowing you to use letter keys as up, down, left, right,
+--- delete, return, home, and end so your hands can comfortably
+--- stay within the range of the letter keys.
 
-local spaceDown = false
-local normalKey = ""
-local produceSpace = true
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "TouchCursor"
+obj.version = "0.1"
+obj.author = "Kevin Li <kevinli020508@gmail.com>"
+obj.homepage = "https://github.com/AlienKevin/touchcursor-macos"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+obj.spaceDown = false
+obj.normalKey = ""
+obj.produceSpace = true
 
 -- listen to keypress on modifiers
 -- eventtap.new({events.flagsChanged}, function(event)
 --     print(event:getFlags())
 -- end):start()
 
-eventtap.new({ events.keyDown }, function(event)
-    local currKey = keycodes.map[event:getKeyCode()]
-    -- print(currKey .. " is down".." normalKey is " .. normalKey)
-    if currKey == normalKey then
-        if normalKey == "space" then
-            -- print("generate space up")
-            hs.eventtap.event.newKeyEvent("space", false):post()
+function obj:init()
+    hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function(event)
+        local currKey = hs.keycodes.map[event:getKeyCode()]
+        -- print(currKey .. " is down".." normalKey is " .. normalKey)
+        if currKey == self.normalKey then
+            if self.normalKey == "space" then
+                -- print("generate space up")
+                hs.eventtap.event.newKeyEvent("space", false):post()
+            end
+            return false
         end
-        return false
-    end
-    if currKey == "space" then
-        spaceDown = true
-        produceSpace = true
-        return true
-    end
-    if spaceDown then
-        local newKey = nil
-        if currKey == "i" then
-            newKey = "up"
-        elseif currKey == "j" then
-            newKey = "left"
-        elseif currKey == "k" then
-            newKey = "down"
-        elseif currKey == "l" then
-            newKey = "right"
-        elseif currKey == "p" then
-            newKey = "delete"
-        elseif currKey == "o" then
-            newKey = "end"
-        elseif currKey == "u" then
-            newKey = "home"
-        elseif currKey == "h" then
-            newKey = "return"
-        end
-        if newKey ~= nil then
-            produceSpace = false
-            normalKey = newKey
-            hs.eventtap.event.newKeyEvent(newKey, true):post()
-            hs.eventtap.event.newKeyEvent(newKey, false):post()
+        if currKey == "space" then
+            self.spaceDown = true
+            self.produceSpace = true
             return true
         end
-    end
-    return false
-end):start()
+        if self.spaceDown then
+            local newKey = nil
+            if currKey == "i" then
+                newKey = "up"
+            elseif currKey == "j" then
+                newKey = "left"
+            elseif currKey == "k" then
+                newKey = "down"
+            elseif currKey == "l" then
+                newKey = "right"
+            elseif currKey == "p" then
+                newKey = "delete"
+            elseif currKey == "o" then
+                newKey = "end"
+            elseif currKey == "u" then
+                newKey = "home"
+            elseif currKey == "h" then
+                newKey = "return"
+            end
+            if newKey ~= nil then
+                self.produceSpace = false
+                self.normalKey = newKey
+                hs.eventtap.event.newKeyEvent(newKey, true):post()
+                hs.eventtap.event.newKeyEvent(newKey, false):post()
+                return true
+            end
+        end
+        return false
+    end):start()
 
-eventtap.new({ events.keyUp }, function(event)
-    local currKey = keycodes.map[event:getKeyCode()]
-    -- print(currKey .. " is up".." normalKey is " .. normalKey)
-    if currKey == normalKey then
-        normalKey = ""
-        return false
-    end
-    if currKey == "space" then
-        spaceDown = false
-        normalKey = ""
-        if produceSpace then
-            normalKey = "space"
-            -- print("generate space down")
-            hs.eventtap.event.newKeyEvent("space", true):post()
-            return true
+    hs.eventtap.new({ hs.eventtap.event.types.keyUp }, function(event)
+        local currKey = hs.keycodes.map[event:getKeyCode()]
+        -- print(currKey .. " is up".." normalKey is " .. normalKey)
+        if currKey == self.normalKey then
+            self.normalKey = ""
+            return false
         end
-    end
-    return false
-end):start()
+        if currKey == "space" then
+            self.spaceDown = false
+            self.normalKey = ""
+            if self.produceSpace then
+                self.normalKey = "space"
+                -- print("generate space down")
+                hs.eventtap.event.newKeyEvent("space", true):post()
+                return true
+            end
+        end
+        return false
+    end):start()
+end
+
+return obj
